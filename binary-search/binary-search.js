4f03721a20aee2d8a7ee242196d092666695e0ee
Dan <daniel.sohval@gmail.com>
2019-01-02 12:52:55 -0500

    added binary search solution

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,7 +1,19 @@
 'use strict';
 
 const binarySearch = (array, target, left = 0, right = array.length) => {
-	// your code here
+	let toCheck = Math.floor((left + right) / 2 );
+	if(right < left){
+		return false
+	} 
+	if(array[toCheck] === target){
+		return true	
+	} else if(array[toCheck] > target){
+		right = toCheck - 1
+		return binarySearch(array, target, left, right);
+	} else {
+		left = toCheck + 1
+		return binarySearch(array, target, left, right);
+	}
 };
 
 module.exports = binarySearch
