24f3ad2d2e580ecda5a66f8814be8ee75b0472a3
Daniel Sohval <dsohval@eevo.com>
2019-01-08 17:07:45 -0500

    Update binary-search.spec.js

--- binary-search/binary-search.spec.js
+++ binary-search/binary-search.spec.js
@@ -20,6 +20,6 @@ describe('Binary Search', () => {
 		expect(binarySearch(test3, 4)).to.equal(false);
 	});
 
-	//WRITE YOUR OWN TESTS HERE. Include >=1 edge case :)
+	//(OPTIONAL) WRITE YOUR OWN TESTS HERE. Include >=1 edge case :)
 
 });
