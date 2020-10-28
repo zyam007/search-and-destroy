57a78ca1ccb8a17fca0d6b042edc819a1fec63b2
Yanna Sk <zyam007@gmail.com>
2020-10-28 16:57:04 -0400

    binarysearch done

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,9 +1,28 @@
 'use strict';
-
+//no to use slice, should pass indexes as args to function and keep track of start/end
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+  if (array.length === 1 || array.length === 0) {
+    if (array[0] === target) return true;
+    return false;
+  }
+  let halfPoint = Math.floor(array.length / 2);
+  let halfNum = array[halfPoint];
+
+  if (halfNum === target) {
+    return true;
+  } else {
+    if (target < halfNum) {
+      return binarySearch(array.slice(0, halfPoint), target);
+    }
+    if (target > halfNum) {
+      return binarySearch(array.slice(halfPoint + 1), target);
+    }
+  }
 };
+
+//[1,3,4,5,10,15,20,22,23,98,1000] //11 elemtns
+//target 10
 
 /*
 	EXTRA CREDIT:
@@ -14,4 +33,4 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
--- binary-search/binary-search.spec.js
+++ binary-search/binary-search.spec.js
@@ -1,25 +1,27 @@
 'use strict';
-const {expect} = require('chai');
+const { expect } = require('chai');
 const binarySearch = require('./binary-search');
 
-const test1 = [1,3,4,5,10,15,20,22,23,98,1000];
-const test2 = [-3,-2,0, 9, 10, 11, 12, 15, 17, 18, 19, 20];
+const test1 = [1, 3, 4, 5, 10, 15, 20, 22, 23, 98, 1000];
+const test2 = [-3, -2, 0, 9, 10, 11, 12, 15, 17, 18, 19, 20];
 const test3 = [-0.5, 0.03, 2.6, 3, 3.5, 3.777777, 5];
-
-
+const test4 = [];
+const test5 = [3];
 describe('Binary Search', () => {
-	it('returns true if target value is in array', () =>{
-		expect(binarySearch(test1, 10)).to.equal(true);
-		expect(binarySearch(test2, -3)).to.equal(true);
-		expect(binarySearch(test2, 20)).to.equal(true);
-		expect(binarySearch(test3, 2.6)).to.equal(true);
-	});
+  it('returns true if target value is in array', () => {
+    expect(binarySearch(test1, 10)).to.equal(true);
+    expect(binarySearch(test2, -3)).to.equal(true);
+    expect(binarySearch(test2, 20)).to.equal(true);
+    expect(binarySearch(test3, 2.6)).to.equal(true);
+    expect(binarySearch(test5, 3)).to.equal(true);
+  });
 
-	it('returns false if target value is not in array', () => {
-		expect(binarySearch(test2, 25)).to.equal(false);
-		expect(binarySearch(test3, 4)).to.equal(false);
-	});
+  it('returns false if target value is not in array', () => {
+    expect(binarySearch(test2, 25)).to.equal(false);
+    expect(binarySearch(test3, 4)).to.equal(false);
+    expect(binarySearch(test4, 2)).to.equal(false);
+    expect(binarySearch(test5, 2)).to.equal(false);
+  });
 
-	//(OPTIONAL) WRITE YOUR OWN TESTS HERE. Include >=1 edge case :)
-
+  //(OPTIONAL) WRITE YOUR OWN TESTS HERE. Include >=1 edge case :)
 });
